90a711ddf1dc1cad028f1aa708fa251ceabb88c3
Jwalin Shah <209190755+jwalin-shah@users.noreply.github.com>
2026-06-15 09:13:04 -0700

    m3-pipeline: impl/init-with-keys

--- src/api_key_router.py
+++ src/api_key_router.py
@@ -1,22 +1,5 @@
 class ApiKeyRouter:
-    def __init__(self, keys):
-        self._all_keys = list(keys)
+    def __init__(self, keys: list) -> None:
+        self._keys = keys
         self._exhausted = set()
-
-    def mark_quota_exceeded(self, key):
-        if key in self._all_keys:
-            self._exhausted.add(key)
-
-    def available_keys(self):
-        return [k for k in self._all_keys if k not in self._exhausted]
-
-    def next_key(self):
-        available = self.available_keys()
-        if not available:
-            return None
-        return available[0]
-
-    def reset_key(self, key):
-        if key not in self._all_keys:
-            return
-        self._exhausted.discard(key)
+        self._cursor = 0
