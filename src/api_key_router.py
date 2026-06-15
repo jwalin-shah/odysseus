4d22a26fa018fe4910e0e934005d428912322c07
Jwalin Shah <209190755+jwalin-shah@users.noreply.github.com>
2026-06-15 09:13:07 -0700

    m3-pipeline: impl/mark-quota-error

--- src/api_key_router.py
+++ src/api_key_router.py
@@ -1,5 +1,10 @@
 class ApiKeyRouter:
-    def __init__(self, keys: list) -> None:
-        self._keys = keys
+    def __init__(self, keys):
+        self._keys = list(keys)
         self._exhausted = set()
-        self._cursor = 0
+
+    def mark_quota_error(self, key: str) -> None:
+        self._exhausted.add(key)
+
+    def available_keys(self):
+        return [k for k in self._keys if k not in self._exhausted]
