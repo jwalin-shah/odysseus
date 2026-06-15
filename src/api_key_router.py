3a7badd7e6da34b2faaf5ff762d69ba483238566
Jwalin Shah <209190755+jwalin-shah@users.noreply.github.com>
2026-06-15 08:31:11 -0700

    m3-pipeline: impl/router-reset-key

--- src/api_key_router.py
+++ src/api_key_router.py
@@ -1,17 +1,22 @@
 class ApiKeyRouter:
     def __init__(self, keys):
-        self._keys = list(keys)
+        self._all_keys = list(keys)
         self._exhausted = set()
-        self._index = 0
+
+    def mark_quota_exceeded(self, key):
+        if key in self._all_keys:
+            self._exhausted.add(key)
 
     def available_keys(self):
-        return [k for k in self._keys if k not in self._exhausted]
+        return [k for k in self._all_keys if k not in self._exhausted]
 
-    def mark_quota_exceeded(self, key: str) -> str | None:
-        self._exhausted.add(key)
-        self._index += 1
-        while self._index < len(self._keys):
-            if self._keys[self._index] not in self._exhausted:
-                return self._keys[self._index]
-            self._index += 1
-        return None
+    def next_key(self):
+        available = self.available_keys()
+        if not available:
+            return None
+        return available[0]
+
+    def reset_key(self, key):
+        if key not in self._all_keys:
+            return
+        self._exhausted.discard(key)
