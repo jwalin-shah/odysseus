0457f1808384a6ea303e2b4562c458fa74ac1eb9
Jwalin Shah <209190755+jwalin-shah@users.noreply.github.com>
2026-06-15 10:41:23 -0700

    m3-pipeline: impl/dedup-queue-push

--- src/dedup_queue.py
+++ src/dedup_queue.py
@@ -6,20 +6,15 @@ class DedupQueue:
         self._items = []
         self._seen_keys = set()
 
-    def push(self, item, key) -> bool:
+    def push(self, item: object, key: object) -> bool:
         if key in self._seen_keys:
             return False
         self._seen_keys.add(key)
         self._items.append(item)
         return True
 
-    def peek(self) -> object:
-        if not self._items:
-            raise IndexError("peek from empty DedupQueue")
-        return self._items[0]
+    def __iter__(self) -> Iterator[object]:
+        return iter(self._items)
 
     def __len__(self) -> int:
         return len(self._items)
-
-    def __iter__(self) -> Iterator[object]:
-        return iter(self._items)
