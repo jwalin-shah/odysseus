e2c6f3cdd2a3a7f0e8bde1325b738e181e4d4314
Jwalin Shah <209190755+jwalin-shah@users.noreply.github.com>
2026-06-15 08:30:51 -0700

    m3-pipeline: impl/router-next-key

--- src/api_key_router.py
+++ src/api_key_router.py
@@ -1,18 +1,24 @@
 class ApiKeyRouter:
-    def __init__(self, keys: list[str]) -> None:
-        self._keys = list(keys)
-        self._index = 0
-
-    def available_keys(self) -> list[str]:
-        return self._keys
-
-    def current_key(self) -> str | None:
-        if not self._keys:
-            return None
-        return self._keys[self._index]
+    def __init__(self, keys: list[str]):
+        self.keys = keys
+        self.exhausted = set()
+        self.index = 0
 
     def next_key(self) -> str | None:
-        if not self._keys:
+        if not self.keys:
             return None
-        self._index = (self._index + 1) % len(self._keys)
-        return self._keys[self._index]
+
+        if len(self.exhausted) >= len(self.keys):
+            return None
+
+        start = self.index
+        while True:
+            key = self.keys[self.index]
+            self.index = (self.index + 1) % len(self.keys)
+            if key not in self.exhausted:
+                return key
+            if self.index == start:
+                return None
+
+    def mark_quota_exceeded(self, key: str) -> None:
+        self.exhausted.add(key)
